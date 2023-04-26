1fd86f3d65b2dd438afda67515b06f57b063439e
Alexandr Krivosheya <a.krivosheya@g.nsu.ru>
2023-04-26 13:56:03 +0700

    Add base class

--- DistrHashMap.py
+++ DistrHashMap.py
@@ -22,4 +22,26 @@ class DistrHashMap:
 
     def __localPop(self, key):
         self.__hashMap.pop(hash(key))
+
+class AtomicOperations(DistrHashMap):
+    def __init__(self, ownHost, otherHosts):
+        DistrHashMap.__init__(self, ownHost, otherHosts)
+        self.__localSet('lock', True)
+        self.__localSet('owner', None)
+
+    def compareAndSwap(self, reg, oldValue, newValue):
+        self.__lock()
+        if(reg.value == oldValue):
+            reg.set(newValue)
+            self.__release()
+            return True
+        else:
+            self.__release()
+            return False
+        
+    def __lock(self):
+        pass
+
+    def __release(self):
+        pass
         
